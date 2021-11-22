0dc61c428da86fc2b6040f968e356fffb85f1236
MaffPerez <plzamfr@gmail.com>
2021-11-22 18:42:53 -0500

    Add views

--- reto-tech/src/app/app.module.ts
+++ reto-tech/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+import { HomeComponent } from './views/home/home.component';
+import { ShoppingCartComponent } from './views/shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule,
