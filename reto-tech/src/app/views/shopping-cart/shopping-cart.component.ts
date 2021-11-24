837291e303b0227958067244ea9da3411ae57e53
MaffPerez <plzamfr@gmail.com>
2021-11-24 13:18:31 -0500

    Create function getTotalAmount and deleteProduct

--- reto-tech/src/app/views/shopping-cart/shopping-cart.component.ts
+++ reto-tech/src/app/views/shopping-cart/shopping-cart.component.ts
@@ -7,12 +7,15 @@ import { Router } from '@angular/router';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor(private router: Router) { }
-
+  base: number = 1;
+  total: number = 0;
   cart: any[] = [];
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.getCart();
+    this.getTotalAmount();
   }
 
   btnBack() {
@@ -25,7 +28,23 @@ export class ShoppingCartComponent implements OnInit {
     console.log(dataCart);
   }
 
-  changeAmount(){
-    
+  changeQuantity(base:number, item:any){
+    if (item.amount + base < 1) {
+      item.amount === 1;
+    } else {
+      item.amount += base;
+    }
+    this.getTotalAmount();
+  }
+
+  getTotalAmount(){
+    this.total = this.cart
+    .map((item: { amount: number; data: { price:number} }) => item.amount * item.data.price)
+    .reduce((acc: any, item: any) => (acc += item), 0);
+  }
+
+  deleteProduct(id:string){
+    this.cart = this.cart.filter((product:{data:{id:string} }) => product.data.id !== id)
+    this.getTotalAmount();
   }
 }
